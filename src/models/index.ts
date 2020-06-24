6f51ebda79a0cb2cf5fb1f13a4289e820c9746aa
peroLuo <1025558554@qq.com>
2020-06-24 12:23:53 +0800

    close

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,6 @@
 import { dolphins } from "./dolphins";
 import { sharks } from "./sharks";
 
-// no need to extend from Models
 export interface RootModel {
   dolphins: typeof dolphins;
   sharks: typeof sharks;
